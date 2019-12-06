d46ba04a9557b784470e4d6cfcf52f9a01e9737b
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-12-06 00:29:25 -0300

    importando tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
 
 import { Container } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <Header></Header>
+      <Header />
+
+      <Tabs />
     </Container>
   );
 }
