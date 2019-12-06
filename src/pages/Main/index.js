788fe37d400f33d8c8fa3c992eaea6a52535611e
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-12-06 01:39:54 -0300

    importando menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,6 +3,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 import { Container, Card, Content, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
@@ -12,6 +13,8 @@ export default function Main() {
       <Header />
 
       <Content>
+        <Menu />
+
         <Card>
           <CardHeader>
             <Icon name="attach-money" size={28} color="#666" />
